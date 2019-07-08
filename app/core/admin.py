a14578d6e8b65bba2b9f75f0ea6d5cc603c3fbf0
Andres Vargas <zodman@gmail.com>
2019-07-08 16:27:33 -0500

    first commit

---
+++ app/core/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Car, Client, Personal, Visit, Diagnostic, Release
+from .models import Quote, Item
+
+
+admin.site.register(Car)
+admin.site.register(Client)
+admin.site.register(Personal)
+admin.site.register(Visit)
+admin.site.register(Diagnostic)
+admin.site.register(Release)
+admin.site.register(Quote)
+admin.site.register(Item)
---
+++ app/core/models.py
@@ -0,0 +1,70 @@
+from django.db import models
+
+
+class Personal(models.Model):
+    name = models.CharField(max_length=200)
+    position = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Client(models.Model):
+    name = models.CharField(max_length=200)
+    phone = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class Car(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    brand = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.brand} -- {self.model}"
+
+
+class Visit(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    diagnostic = models.ForeignKey("Diagnostic", on_delete=models.CASCADE)
+    release = models.ForeignKey("Release", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Diagnostic(models.Model):
+    reception_datetime = models.DateTimeField()
+    initial = models.TextField()
+    final = models.TextField()
+    repairs = models.TextField()
+    notes = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Item(models.Model):
+    quantity = models.PositiveIntegerField()
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=8)
+    quote = models.ForeignKey("Quote", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Quote(models.Model):
+    client = models.ForeignKey("Client", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+class Release(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    quotes = models.ManyToManyField("Quote")
