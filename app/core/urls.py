1b0d46baf8b35159111b6322b6aee8d1a8ef2572
Andres Vargas <zodman@gmail.com>
2019-07-12 15:05:55 -0500

    add cars

--- app/core/urls.py
+++ app/core/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path("client/add", client_add, name="client_add"),
     path("client/search/", search_client, name="search_client"),
     path("client/delete/", delete_clients, name="delete_clients"),
-
     path("cars/", car_list, name="car_list"),
-    path("car/add", car_add, name="car_add"),
-    path("cars/search/", search_car_clients, name="search_car_clients"),
+    path("cars/add/", car_add, name="car_add"),
+    path("cars/search/", search_car, name="search_car"),
+    path("cars/search/client/", search_car_clients, name="car_client_search"),
 ]
--- app/core/views/cars.py
+++ app/core/views/cars.py
@@ -2,35 +2,59 @@ from django.shortcuts import render
 from django.views.generic import ListView, CreateView
 from django.urls import reverse_lazy as reverse
 from django.contrib import messages
-from ..models import Car
+from django.http import HttpResponse
+from ..models import Car, Client
 from django.db.models import Q
 from .utils import CreateIntercoolerMix
-
-__all__=["search_car_clients", "car_list", "car_add"]
+import json
+__all__ = ["search_car_clients", "search_car", "car_list", "car_add"]
 
 
 class CarAdd(CreateIntercoolerMix):
-    model  = Car
+    model = Car
     fields = ("brand","model","year")
+#    fields = ("__all__")
+
+    def form_valid(self, form):
+        client_id = self.request.POST.get("client_id")
+        if not client_id:
+            form.add_error(None, "Missing client")
+            return super().form_invalid(form)
+        else:
+            instance = form.save(commit=False)
+            instance.client = Client.objects.get(id=client_id)
+            instance.save()
+        return super().form_valid()
 
 car_add = CarAdd.as_view()
 
 
-def search_car_clients(request):
+def search_car(request):
     search = request.GET.get("search")
     if search:
         cars = Car.objects.filter(
-                Q(client__name__icontains=search)|
-                Q(brand__icontains=search)|
-                Q(model__icontains=search))
+            Q(client__name__icontains=search) | Q(brand__icontains=search)
+            | Q(model__icontains=search))
     else:
         cars = Car.objects.none()
     context = dict(object_list=cars)
     return render(request, "core/_cars.html", context)
 
 
+def search_car_clients(request):
+    search = request.GET.get("search")
+    if search:
+        cars = Client.objects.filter(Q(name__icontains=search))
+    else:
+        cars = Client.objects.none()
+
+    resp = [{'value': car.id, 'label': car.name} for car in cars]
+    return HttpResponse(json.dumps(resp))
+
+
 class CarList(ListView):
     model = Car
     paginate_by = 20
+
 
 car_list = CarList.as_view()
