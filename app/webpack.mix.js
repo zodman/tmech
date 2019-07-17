9142847d5e143d71cdae379b67650a7d341e81d5
Andres Vargas <zodman@gmail.com>
2019-07-17 17:53:07 -0500

    update data

--- app/webpack.mix.js
+++ app/webpack.mix.js
@@ -15,6 +15,7 @@ let mix = require('laravel-mix');
 mix.setPublicPath('core/static/');
 mix.js('core/static/src/app.js', 'core/static/dist/')
 mix.sass('core/static/src/app.scss', 'core/static/dist/')
+mix.options({ processCssUrls: false });
 
 // Full API
 // mix.js(src, output);
