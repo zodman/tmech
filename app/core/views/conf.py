6d58fe5e0e210c864dc81b0d45f004ab589bc793
zodman <zodman@gmail.com>
2019-08-08 23:45:33 -0500

    setconf

--- app/core/views/conf.py
+++ app/core/views/conf.py
@@ -1,18 +1,28 @@
 from django.forms import modelform_factory
 from core.models import Conf
 from django.shortcuts import render
+from django.contrib import messages
+from django.utils.translation import gettext_lazy as _
+
 
 def setconf(request):
     ConfForm = modelform_factory(Conf, fields=("name","logo"))
-    
+    try:
+        instance = getattr(request.user,"conf")
+    except Conf.DoesNotExist:
+        instance = None
     if request.POST:
-        form = ConfForm(request.POST)
+        if instance:
+            form = ConfForm(request.POST, instance=instance)
+        else:
+            form  = ConfForm(request.POST)
         if form.is_valid():
             instance=form.save(commit=False)
             instance.user = request.user
             instance.save()
+            messages.info(request, _("Conf set"))
     else:
-        form = ConfForm()
+        form = ConfForm(instance=instance)
     context = {
         'form':form
     }
