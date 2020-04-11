9d9ac350afa443574cd72e7f6850e923fc16af37
pelana kun <pelana@hushmail.com>
2020-04-11 00:25:42 -0500

    update pipfile

--- app/app/settings.py
+++ app/app/settings.py
@@ -192,3 +192,14 @@ MESSAGE_TAGS = {
 }
 
 PAYPAL_BUSSINES="zodman@gmail.com"
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://cbbb049b938c41cf9721fade81fd2754@o186464.ingest.sentry.io/5196843",
+    integrations=[DjangoIntegration()],
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
